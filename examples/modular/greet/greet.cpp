a7442fd7f4417fed2ec4e7434f5064e7eea03fdb
Stefan Seefeld <stefan@seefeld.name>
2016-11-06 22:23:00 -0500

    Add modular example.

---
+++ examples/modular/greet/greet.cpp
@@ -0,0 +1,15 @@
+//
+// Copyright (c) 2016 Stefan Seefeld
+// All rights reserved.
+//
+// This file is part of Faber. It is made available under the
+// Boost Software License, Version 1.0.
+// (Consult LICENSE or http://www.boost.org/LICENSE_1_0.txt)
+
+#include "greet.hpp"
+#include <iostream>
+
+GREET_API void greet()
+{
+  std::cout << "Hello!" << std::endl;
+}
