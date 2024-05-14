efdcc22f90a372c0504c7fb44f4663ec04d02771
Stefan Seefeld <stefan@seefeld.name>
2024-05-14 17:00:07 +0200

    upgrade versioneer

--- src/faber/__init__.py
+++ src/faber/__init__.py
@@ -6,8 +6,7 @@
 # Boost Software License, Version 1.0.
 # (Consult LICENSE or http://www.boost.org/LICENSE_1_0.txt)
 
-from ._version import get_versions
-__version__ = get_versions()['version']
-del get_versions
+from . import _version
+__version__ = _version.get_versions()['version']
 
 debug = False
