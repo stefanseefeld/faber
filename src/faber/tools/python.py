de6ffc8c5513af65a17aa2ef6e633f18192314d3
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2025-04-01 02:12:13 +0900

    fix: always use EXT_SUFFIX
    close #34

--- src/faber/tools/python.py
+++ src/faber/tools/python.py
@@ -117,8 +117,4 @@ class python(tool):
         flags += ' ' + self.check_sysconfig('get_config_var("SHLIBS")')
         flags = [f[2:] for f in flags.strip().split() if f.startswith('-l')]
         self.libs += libs(*flags)
-        version = self.check_sysconfig('get_config_var("py_version")')
-        if version >= '3.5':
-            self.ext_suffix = self.check_sysconfig('get_config_var("EXT_SUFFIX")')
-        else:
-            self.ext_suffix = self.check_sysconfig('get_config_var("SO")')
+        self.ext_suffix = self.check_sysconfig('get_config_var("EXT_SUFFIX")')
