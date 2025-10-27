e5b8d23595a519aa35b51eb9ca015e190dad2888
Stefan Seefeld <stefan@seefeld.name>
2025-10-26 20:09:12 -0400

    switch to new build logic.

--- setup.py
+++ setup.py
@@ -7,26 +7,29 @@
 # Boost Software License, Version 1.0.
 # (Consult LICENSE or http://www.boost.org/LICENSE_1_0.txt)
 
-from setuptools import setup, find_packages
-from distutils.command import build, install_data, sdist
-import versioneer
-import sys
+from setuptools import setup
+from setuptools.command.build import build
+from subprocess import check_output
+import re
 import os
-import subprocess
 
-if sys.version_info < (3, 6, 0):
-    sys.exit('Faber requires Python 3.6 or newer.')
+def get_version_from_git():
+    try:
+        out = check_output('git describe --tags --long --match release/*'.split()).decode().strip()
+        match = re.match(r'release/'
+                         r'(?P<version>[a-zA-Z0-9.]+)'
+                         r'(?:-(?P<post>\d+)-g(?P<hash>[0-9a-f]{7,}))$',
+                         out)
+        version, post, hash = match.groups()
+        return version if post == '0' else '{0}.post{1}+{2}'.format(version, post, hash)
+    except Exception:
+        raise ValueError('unable to extract version from git tag')
 
-# allow the in-place import of the version
-sys.path.insert(0, 'src')
-from faber import cli  # noqa E402
 
-version = versioneer.get_version()
-
-data = [('share/doc/faber-{}'.format(version), ('LICENSE', 'README.md'))]
+data = [('share/doc/faber', ('LICENSE', 'README.md'))]
 
 
-class build_doc(build.build):
+class build_doc(build):
 
     description = "build documentation"
 
@@ -42,35 +45,10 @@ class build_doc(build.build):
 docs = []
 if os.path.exists('doc/html'):
     for root, dirs, files in os.walk('doc/html'):
-        dest = root.replace('doc/html', 'share/doc/faber-{}'.format(version))
+        dest = root.replace('doc/html', 'share/doc/faber')
         docs.append((dest,
                     [os.path.join(root, file) for file in files
                      if os.path.isfile(os.path.join(root, file))]))
 
 
-setup(name='faber',
-      python_requires='>=3.6',
-      version=version,
-      author='Stefan Seefeld',
-      author_email='stefan@seefeld.name',
-      maintainer='Stefan Seefeld',
-      maintainer_email='stefan@seefeld.name',
-      description='Faber is a construction tool.',
-      url='https://stefanseefeld.github.io/faber',
-      download_url='https://github.com/stefanseefeld/faber/releases',
-      license='BSL',
-      classifiers=['Environment :: Console',
-                   'Intended Audience :: Developers',
-                   'License :: OSI Approved :: Boost Software License 1.0 (BSL-1.0)',
-                   'Operating System :: OS Independent',
-                   'Topic :: Software Development :: Build Tools',
-                   'Topic :: Software Development :: Testing',
-                   'Programming Language :: Python'],
-      cmdclass=versioneer.get_cmdclass({'build_doc': build_doc}),
-      package_dir={'': 'src'},
-      packages=find_packages(where='src'),
-      entry_points=dict(console_scripts=['faber=faber.cli:cli_main']),
-      data_files=data + docs,
-      setup_requires=['pytest-runner'],
-      tests_require=['pytest'],
-      )
+setup(data_files=data + docs)
